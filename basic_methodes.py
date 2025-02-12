93b9a79f1ec07911b93c024fa7eedfd51e7593cf
Eclipse <Eclips77@users.noreply.github.com>
2025-02-12 13:33:48 +0200

    add advaced keylogger

--- basic_methodes.py
+++ basic_methodes.py
@@ -1,14 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import List
 class IKeyLogger(ABC):
- @abstractmethod
- def start_logging(self) -> None:
- pass
+  @abstractmethod
+  def start_logging(self) -> None:
+   pass
 
- @abstractmethod
- def stop_logging(self) -> None:
- pass
+  @abstractmethod
+  def stop_logging(self) -> None:
+   pass
 
- @abstractmethod
- def get_logged_keys(self) -> List[str]:
- pass
+  @abstractmethod
+  def get_logged_keys(self) -> List[str]:
+   pass
