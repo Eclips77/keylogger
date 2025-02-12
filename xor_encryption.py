3cb57213ef52d67dadd832d0c46acd008baac6c9
AharoLan <a0548532272@gmail.com>
2025-02-12 15:18:56 +0200

    Establishing a base class for encryption and a subclass for XOR encryption

---
+++ xor_encryption.py
@@ -0,0 +1,21 @@
+from Encryption import Encryption
+from config import SECRET_KEY
+
+class XOREncryption(Encryption):
+    """
+    XOR encryption class.
+    """
+    def __init__(self):
+        self.secret_key = SECRET_KEY
+
+    def encrypt(self, text):
+        encrypted_text = ""
+
+        # Iterate over each character in the text
+        for i in range(len(text)):
+            encrypted_text += chr(ord(text[i]) ^ ord(self.secret_key[i % len(self.secret_key)]))
+
+        return encrypted_text
+
+    def decrypt(self, encrypted_text):
+        return self.encrypt(encrypted_text)
